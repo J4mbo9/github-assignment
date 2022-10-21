3b009f47ba3a403b0dc32dbe4f7c7372a205526e
J4mbo9 <chkfrdlek@naver.com>
2022-10-21 18:05:58 +0900

    todo test

---
+++ src/main/java/com/codestates/GitHubRequirementsApplication.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRequirementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRequirementsApplication.class, args);
+	}
+
+}
